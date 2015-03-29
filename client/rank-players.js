3c609e849afc010aa1e962384df93151b37c18ce
Hugo Ramos <hgramos28@gmail.com>
2015-03-28 21:46:00 -0400

    Adicionados campos para o facebook

--- client/rank-players.js
+++ client/rank-players.js
@@ -2,8 +2,5 @@ Template.rankPlayers.helpers({
     RankPlayers: function() {
 	Meteor.subscribe('listaUtilizadores');
 	return Meteor.users.find();
-    },
-    primeiro_email: function(array_emails){
-	return array_emails[0].address
     }
 })
